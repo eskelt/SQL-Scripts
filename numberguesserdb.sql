ca4b4b990037e16ffb258a0b7b0683de0f80e7f0
Eskelt <diegogpr3@gmail.com>
2023-07-17 00:14:50 +0200

    modif

--- numberguesserdb.sql
+++ numberguesserdb.sql
@@ -11,8 +11,6 @@ CREATE DATABASE "NUMBER_GUESSER"
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Spanish_Spain.1252'
-    LC_CTYPE = 'Spanish_Spain.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
